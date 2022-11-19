ec7dd26829ba92b6f9f89e9736923f42a69fc0c7
Nowman <gebndlk@gmail.com>
2022-11-19 17:20:36 +0500

    Final commit

--- program.py
+++ program.py
@@ -2,6 +2,7 @@ def main():
     print("My first git program")
     print("And I change it every day")
     print("UFO came and added this line")
+    print("FINAL")
 
 if __name__ == "__main__":
     main()
